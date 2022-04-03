d29d7398c828b50017210748cab9eeb37d4e3362
Jake Hunter <jake7hunter@hmail.com>
2022-04-03 16:10:32 -0400

    Updated

--- src/components/Members.js
+++ src/components/Members.js
@@ -7,7 +7,7 @@ function Members() {
         <Family name="Josh" age={27} birthday="June 12" favfood="Tacos" />
         <Family name="Emily" age={21} birthday="February 23" favfood="Cake" />
         <Family name="Sherry" age={54} birthday="December 25" favfood="Unsalted Peanuts" />
-        <Family name="Tim" age={54} birthday="December 25" favfood="Unsalted Peanuts" />
+        <Family name="Tim" age={54} birthday="August 1" favfood="Egg Sandwich" />
         <Family name="Molly" age={9} birthday="July 10" favfood="Treats" />
         </div>
     );
